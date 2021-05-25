9fb5f175e7a6126347f0f6e9c9f7b73877f5d2d5
szwerver <szwerver@ossur.com>
2021-05-25 10:45:29 +0000

    removed communication handler, not needed

--- CommunicationHandler.py
+++ CommunicationHandler.py
@@ -1,10 +1,7 @@
 import csv
-from ConversionHandler import ConversionHandler
 
 class CommunicationHandler():
     def __init__(self):
-        self.convHandler = ConversionHandler()
-
         # Make csv files for sending/receiving friendly requests
         with open('friendly_request.csv', 'w', newline='') as file:
             writer = csv.writer(file)
